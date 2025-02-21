6018b3cc0a447e01e6e8b5c7ca1d5f195867faa1
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2025-02-20 22:13:12 -0600

    added test

--- tests.py
+++ tests.py
@@ -14,6 +14,19 @@ class Tests(unittest.TestCase):
             len(m1._cells[0]),
             num_rows,
         )
+    
+    def test_maze_big(self):
+        num_cols = 100
+        num_rows = 200
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+                len(m1._cells),
+                num_cols,
+        )
+        self.assertEqual(
+                len(m1._cells[0]),
+                num_rows,
+        )
 
 if __name__ == "__main__":
     unittest.main()
